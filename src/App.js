46335ee22da7c2cfd95d85cbabedee83a79a146f
Anastasia <anastasia.viva.v@gmail.com>
2022-04-26 18:25:38 +0500

    router-init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import MainChatPage from "./pages";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <MainChatPage />
+      <Routes>
+        <Route path="/chat" element={<MainChatPage />} />
+        <Route path="*" element={<Navigate to="/chat" />} />
+      </Routes>
     </div>
   );
 }
